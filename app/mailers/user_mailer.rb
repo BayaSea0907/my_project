7e39d6bc49aa4445361773bda2d4573b1d0cbe3a
Sea Baya <seabaya@shinoMacBook-Air.local>
2022-02-18 02:32:30 +0900

    `rails generate sorcery:install remember_me reset_password` & migration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,24 @@
 class UserMailer < ApplicationMailer
   default from: 'test@example.com'
 
-  def hoge_email
-    @user = params[:user]
-    @url  = 'localhost:3000/login'
-    mail(to: @user.email, subject: 'MyProjectへようこそ')
+  def activation_needed_email(user)
+    @user = user
+    @url  = "http://0.0.0.0:3000/users/#{user.activation_code}/activate"
+    mail(to: user.email,
+         subject: 'Welcome to My Awesome Site')
+  end
+
+  def activation_success_email(user)
+    @user = user
+    @url  = 'http://0.0.0.0:3000/login'
+    mail(to: user.email,
+         subject: 'Your account is now activated')
+  end
+
+  def reset_password_email(user)
+    @user = user
+    @url  = "http://0.0.0.0:3000/password_resets/#{user.reset_password_token}/edit"
+    mail(to: user.email,
+         subject: 'Your password reset request')
   end
 end
