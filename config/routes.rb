39dd870e3ff972ce89e00089a2599e829fc3fe02
Sea Baya <seabaya@shinoMacBook-Air.local>
2022-02-19 02:51:33 +0900

    トップページの作成と、bulmaのナビゲーションメニュー導入。

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root :to => 'users#index'
-  resources :users
+  root :to => 'top#index'
 
+  resources :users
   resources :password_resets, only: [:create, :edit, :update]
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => "user_sessions#create"
-  post 'logout' => 'user_sessions#destroy', :as => :logout
+  get 'logout' => 'user_sessions#destroy', :as => :logout
 end
