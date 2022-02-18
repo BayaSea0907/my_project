1af95d43770be6914a2cd22bd5bbc8a7fc936520
Sea Baya <seabaya@shinoMacBook-Air.local>
2022-02-19 01:46:18 +0900

    annotateの導入

--- db/migrate/20220217165723_sorcery_core.rb
+++ db/migrate/20220217165723_sorcery_core.rb
@@ -1,11 +1,11 @@
 class SorceryCore < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :email,            null: false, index: { unique: true }
-      t.string :crypted_password
-      t.string :salt
+      t.string :email, null: false, index: { unique: true }, comment: 'メールアドレス'
+      t.string :crypted_password, comment: '暗号化済パスワード'
+      t.string :salt, comment: 'パスワードハッシュ'
 
-      t.timestamps                null: false
+      t.timestamps null: false
     end
   end
 end
--- db/migrate/20220217165910_sorcery_reset_password.rb
+++ db/migrate/20220217165910_sorcery_reset_password.rb
@@ -1,9 +1,9 @@
 class SorceryResetPassword < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :reset_password_token, :string, default: nil
-    add_column :users, :reset_password_token_expires_at, :datetime, default: nil
-    add_column :users, :reset_password_email_sent_at, :datetime, default: nil
-    add_column :users, :access_count_to_reset_password_page, :integer, default: 0
+    add_column :users, :reset_password_token, :string, default: nil, comment: 'パスワードリセットトークン'
+    add_column :users, :reset_password_token_expires_at, :datetime, default: nil, comment: 'パスワードリセットトークン有効期限'
+    add_column :users, :reset_password_email_sent_at, :datetime, default: nil, comment: 'パスワードリセットメール送信日時'
+    add_column :users, :access_count_to_reset_password_page, :integer, default: 0, comment: 'パスワードリセットページアクセス数'
 
     add_index :users, :reset_password_token
   end
